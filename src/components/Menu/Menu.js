4ab71fc9c16354b2697d0f1d1f3688dcbb8a9c95
monstaro <monstaro32@gmail.com>
2020-03-17 16:26:02 -0600

    Refactors menu from functional to class component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Menu.css'
 
-const Menu = (props) => {
-  console.log(props)
-  const subjects = ['local', 'entertainment', 'health', 'science', 'technology']
-  return (
+// const Menu = (props) => {
+//   console.log(props)
+//   const subjects = ['local', 'entertainment', 'health', 'science', 'technology']
+//   return (
+//     <nav className="nav-bar">
+//     {
+//       subjects.map(subject => {
+//         return (
+//           <p className="nav-categories"> 
+//           <button onClick={() => props.handleClick(subject)}className="news-type">{subject}</button>
+//         </p>    
+//         )
+//       })
+//     }        
+//     </nav>
+//   )
+// }
+
+class Menu extends Component {
+  constructor() {
+    super()
+    this.state = {
+      subject: ''
+    }
+  }
+  updateState = (event) => {
+    this.setState(
+      {
+        subject: event.target.value
+      }
+    )
+    this.props.handleClick(event.target.value)
+  }
+  render() {
+    return (
     <nav className="nav-bar">
-    {
-      subjects.map(subject => {
-        return (
-          <p className="nav-categories"> 
-          <button onClick={() => props.handleClick(subject)}className="news-type">{subject}</button>
-        </p>    
-        )
-      })
-    }        
+      {
+        Object.keys(this.props.categories).map(subject => {
+          return (
+            <p className="nav-categories">
+              <button onClick={this.updateState} value={subject}className="news-type">{subject}</button>
+            </p>
+          )
+        })
+      }
     </nav>
-  )
+    )
+  }
 }
 
 
