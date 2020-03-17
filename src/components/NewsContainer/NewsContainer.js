98e351036979d3c305a8bfd91c15b1616ed588cd
monstaro <monstaro32@gmail.com>
2020-03-16 19:51:43 -0600

    Passes updated props down

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -4,7 +4,7 @@ import NewsArticle from '../NewsArticle/NewsArticle.js'
 
 
 const NewsContainer = ({type}) => {
-  console.log(Object.keys(type))
+  // console.log(Object.keys(type))
   return (
     <section className="news-container">
       <h1>{Object.keys(type)} news</h1>
