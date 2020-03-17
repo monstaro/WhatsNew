95cdcd3b59d3edb058014e5a78994c9bcf6eda3b
monstaro <monstaro32@gmail.com>
2020-03-16 18:20:24 -0600

    Imports all news categories

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
+import science from '../../data/science';
+import technology from '../../data/technology';
+
+
 import NewsContainer from '../NewsContainer/NewsContainer.js'
 import './App.css';
 
