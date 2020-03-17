351d0e2f2ecd9f733aad9cebe800f1dce8fcf0c8
monstaro <monstaro32@gmail.com>
2020-03-17 17:46:38 -0600

    Adds header that displays site name

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Header/Header.css'
+
+const Header = () => {
+  return (
+    <section className="site-title">
+      <span className='what'>WHATS </span><span className='new'>NEW?</span>
+    </section>
+  )
+}
+
+export default Header;
