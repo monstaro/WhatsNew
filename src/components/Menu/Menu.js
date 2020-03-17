557e62a8e9d9c9bf95ea7684aa99ea692b951f4c
monstaro <monstaro32@gmail.com>
2020-03-16 19:50:49 -0600

    Begins navbar

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import './Menu.css'
 
-// MENU COMPONENT CODE GOES HERE
+const Menu = () => {
+  const subjects = ['local', 'entertainment', 'health', 'science', 'technology']
+  return (
+    <nav className="nav-bar">
+    {
+      subjects.map(subject => {
+        return (
+          <p className="nav-categories"> 
+          <button className="news-type">{subject}</button>
+        </p>    
+        )
+      })
+    }        
+    </nav>
+  )
+}
+
 
 export default Menu;
