16ed6748745949d4cc2de8eee7b69a30bd3bf6ac
monstaro <monstaro32@gmail.com>
2020-03-16 18:16:03 -0600

    Adds h1 saying what type of news we are looking at

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -4,8 +4,10 @@ import NewsArticle from '../NewsArticle/NewsArticle.js'
 
 
 const NewsContainer = ({type}) => {
+  console.log(Object.keys(type))
   return (
     <section className="news-container">
+      <h1>{Object.keys(type)} news</h1>
       <NewsArticle info={type}/>
     </section>
   )
