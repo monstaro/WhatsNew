fdf13486ee2c4e8fa5d7cadbd02b03db403ad25b
monstaro <monstaro32@gmail.com>
2020-03-16 19:50:29 -0600

    Adjusts state format from string to object

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
-import entertainment from '../../data/entertainment';
-import health from '../../data/health';
-import science from '../../data/science';
-import technology from '../../data/technology';
+// import entertainment from '../../data/entertainment';
+// import health from '../../data/health';
+// import science from '../../data/science';
+// import technology from '../../data/technology';
 
 
-import NewsContainer from '../NewsContainer/NewsContainer.js'
+import NewsContainer from '../NewsContainer/NewsContainer.js';
+
+import Menu from '../Menu/Menu.js';
+
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      newsType: local
     }
+    // {
+    //   local: local,
+    //   entertainment: entertainment,
+    //   health: health,
+    //   science: science,
+    //   technology: technology
+    // }
   }
-
+  // setState(category) {
+  //   this.setState(return this.category)
+  // }
   render () {
     return (
       <div className="app">
+        <Menu />
         <NewsContainer type={this.state}/>
       </div>
     );
