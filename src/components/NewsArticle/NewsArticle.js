9bd6516f2d9ac3154993a4b09d3c193a1f984d18
monstaro <monstaro32@gmail.com>
2020-03-16 18:14:38 -0600

    Adds full article link to component

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -6,14 +6,17 @@ const NewsArticle = ({info}) => {
   let local = info.local
   return (
     local.map(item => 
-      <article className="news-article">
-        <h2 class="headline">
+      <article className="news-article" key={item.id}>
+        <h3 class="headline">
           {item.headline}
-        </h2>
+        </h3>
         <img class="image" src={item.img} alt="alt"/>
         <p class="description">
           {item.description}
         </p>
+        <button class="article-link">
+          <a href={item.url}>Link To Article</a>
+        </button>
       </article>
    )
   )
