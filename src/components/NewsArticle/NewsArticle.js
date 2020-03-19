56a2ae64974308909c52783824f70b799c667044
monstaro <monstaro32@gmail.com>
2020-03-18 20:12:20 -0600

    Adds conditional for current news

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import './NewsArticle.css';
+import next from '../../images/next.svg'
 
 const NewsArticle = ({info}) => {
-  return (
-    info.map(item => 
-      <article className="news-article" key={item.id}>
-        <h3 className="headline">
-          {item.headline}
-        </h3>
-        <img className="image" src={item.img} alt="alt"/>
-        <p className="description">
-          {item.description}
-        </p>
-        <button className="article-link">
-          <a href={item.url}>Link To Article</a>
-        </button>
-      </article>
-   )
-  )
+  if (info) {
+    return (
+      info.map(item => 
+        <article className="news-article" key={item.id}>
+          <h3 className="headline">
+            {item.headline}
+          </h3>
+          <img className="image" src={item.img} alt="alt"/>
+          <p className="description">
+            {item.description}
+          </p>
+          <button className="article-link">
+            <a href={item.url}><span className="link-to-article">Link To Article</span> <img className="link-arrow" src={next} alt=""/></a>
+            
+          </button>
+        </article>
+     )
+    )
+  } else {
+    return (
+      <p>Loading...</p>
+    )
+  }
 }
 
 
