cae98dba4980c7bd6ea9ffd48808350e6d6df558
monstaro <monstaro32@gmail.com>
2020-03-19 15:18:21 -0600

    begins testing for renders

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 import App from './App';
 // import NewsContainer from '../NewsContainer/NewsContainer';
 
@@ -11,11 +13,20 @@ describe('App', () => {
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
-  
-  it('has state', () => {
-    expect(App.state)
+  it('should display a news type on button press', () => {
+    const { getByPlaceholderText, getByText, getByDisplayValue } = render(
+      <App />
+    )
+    fireEvent.click(getByDisplayValue('entertainment'))
+    expect(getByText('entertainment')).toBeInTheDocument()
   })
-
+  it('should display a news type on seawrch', () => {
+    const { getByPlaceholderText, getByText, getByDisplayValue } = render(
+      <App />
+    )
+    fireEvent.change(getByPlaceholderText('search news type'), {target: {value: 'entertainment'}})
+    expect(getByText('entertainment')).toBeInTheDocument()
+  })
   // it('should render a container', () => {
   //   render(
   //     <NewsContainer />
