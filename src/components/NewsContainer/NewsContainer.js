5021103be7b83655113a8aae8ffcf694e05dd544
monstaro <monstaro32@gmail.com>
2020-03-19 15:21:27 -0600

    removes 'news' h1

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './NewsContainer.css'
 import NewsArticle from '../NewsArticle/NewsArticle.js'
+import Searchbar from '../Searchbar/Searchbar.js'
 
 
-const NewsContainer = ({type}) => {
+const NewsContainer = (props) => {
   return (
     <section className="news-container">
-      <h1> news</h1>
-      <NewsArticle info={type}/>
+      <section className="search-box">
+       <Searchbar className="search" handleChange={props.handleChange} />
+       </section>
+       <section className="news-section">
+      <NewsArticle info={props.type}/>
+      </section>
     </section>
   )
 }
